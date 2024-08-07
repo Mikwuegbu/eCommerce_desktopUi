765d20af90b9d3a0a2e80e54ec98a7d92053c0c4
Mikwuegbu <mstephen130@yahoo.com>
2024-08-07 17:41:06 +0100

    proe

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
 	return (
 		<div className='text-center'>
-			<h1>STH went wrong, please check the route...</h1>
 			<p>Please go back to the previous page or contact support.</p>
-			<Link to='/'>Home</Link>
+			<Link to='/products' className='underline'>
+				Home
+			</Link>
 		</div>
 	);
 };
