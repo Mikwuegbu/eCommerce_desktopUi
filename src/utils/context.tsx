bdcbeb4b999c9ab454577ad55929858ac84ccada
Mikwuegbu <mstephen130@yahoo.com>
2024-08-14 19:19:09 +0100

    refactor

--- src/utils/context.tsx
+++ src/utils/context.tsx
@@ -1,28 +1,4 @@
-import {
-	createContext,
-	useReducer,
-	Dispatch,
-	Reducer,
-	ReactNode,
-	FC,
-} from "react";
-import { productsType, exploreType } from "../types/products";
-
-//context manager
-export const ExploreContext = createContext<exploreContextType | undefined>(
-	undefined
-);
-
-export const ProductContext = createContext<productContextType | undefined>(
-	undefined
-);
-
-export const QuantityContext = createContext<quantityContextType | undefined>(
-	undefined
-);
-
-// reducer Action type
-type exploreAction = { type: exploreType; payload: exploreType };
+import { createContext, Dispatch } from "react";
 
 // context type --this is what is being provided
 type exploreContextType = {
@@ -30,45 +6,12 @@ type exploreContextType = {
 	dispatch: Dispatch<exploreAction>;
 };
 
-// context type --this is what is being provided
-type productContextType = {
-	cartItems: productsType[] | undefined;
-	setCartItems: Dispatch<productsType[]>;
-	products: productsType[];
-};
+//context manager
+export const ExploreContext = createContext<exploreContextType | undefined>(
+	undefined
+);
 
-type quantityContextType = {
-	quantity: number;
-	setQuantity: Dispatch<number>;
-};
+// reducer Action type
+type exploreAction = { type: string; payload: string };
 
-//Reducer --this is the the reducer function
-const exploreReducer: Reducer<exploreType, exploreAction> = (state, action) => {
-	switch (action.type) {
-		case "explore":
-		case "New in":
-		case "Clothing":
-		case "Shoes":
-		case "Accessories":
-		case "Activewear":
-		case "Gifts & Living":
-		case "Inspiration":
-			return action.payload;
-		default:
-			return state;
-	}
-};
-
-const ContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-	const [location, dispatch] = useReducer<Reducer<exploreType, exploreAction>>(
-		exploreReducer,
-		"explore"
-	);
-	return (
-		<ExploreContext.Provider value={{ location, dispatch }}>
-			{children}
-		</ExploreContext.Provider>
-	);
-};
-
-export default ContextProvider;
+//TODO: implement useAuth
